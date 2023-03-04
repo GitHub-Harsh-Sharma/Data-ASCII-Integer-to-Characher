e4d7f41e99343420cd200918dc0d30913d91b2a3
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2023-03-04 08:51:13 +0530

    Add files via upload

---
+++ Dataascii2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Dataascii2 {
+    public static void main(String args[]) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no:");
+        a = sc.nextInt();
+        {
+            System.out.println((char) a);
+        }
+    }
+}
